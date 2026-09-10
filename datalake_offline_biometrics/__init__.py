6abce661a364d5e48863e3e2765cd9ccc341b143
swayam <>
2026-09-11 01:42:43 +0530

    feat(api): expose __version__ and synthetic_face in package root

--- datalake_offline_biometrics/__init__.py
+++ datalake_offline_biometrics/__init__.py
@@ -1,11 +1,15 @@
-"""Offline facial recognition and liveness primitives for Datalake 3.0."""
+"""Offline facial recognition and liveness primitives for NHAI Datalake 3.0."""
+
+__version__ = "1.0.0"
 
 from .engine import AuthenticationResult, OfflineBiometricEngine
+from .image_ops import synthetic_face
 from .liveness import LivenessDetector, LivenessResult
 from .recognition import FaceTemplate, LBPHFaceRecognizer, RecognitionResult
 from .storage import TemplateStore
 
 __all__ = [
+    "__version__",
     "AuthenticationResult",
     "FaceTemplate",
     "LBPHFaceRecognizer",
@@ -14,4 +18,5 @@ __all__ = [
     "OfflineBiometricEngine",
     "RecognitionResult",
     "TemplateStore",
+    "synthetic_face",
 ]
